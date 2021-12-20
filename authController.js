909223cf117de8f1e7ca7824b865e5ec4f150c69
alexander.makarov1 <alexander.makarov@tabby.ai>
2021-12-20 18:02:39 +0200

    added controller

---
+++ authController.js
@@ -0,0 +1,19 @@
+class authController {
+  async registration(req, res) {
+    try {
+    } catch (e) {}
+  }
+
+  async login(req, res) {
+    try {
+    } catch (e) {}
+  }
+
+  async getUsers(req, res) {
+    try {
+      res.json("getUsers");
+    } catch (e) {}
+  }
+}
+
+module.exports = new authController();
--- authRouter.js
+++ authRouter.js
@@ -1,10 +1,13 @@
 const Router = require("express");
+const { getUsers } = require("./authController");
+const controller = require("./authController");
 
 const router = new Router();
 
 // console.log(router);
 
-router.post("/registration");
-router.get("/users");
+router.post("/registration", controller.registration);
+router.get("/login", controller.login);
+router.get("/users", controller.getUsers);
 
 module.exports = router;
