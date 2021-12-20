4f6028bc1daefbeaa245f5636500ac3db89ab7ba
alexander.makarov1 <alexander.makarov@tabby.ai>
2021-12-20 17:24:37 +0200

    init

---
+++ authRouter.js
@@ -0,0 +1,10 @@
+const Router = require("express");
+
+const router = new Router();
+
+// console.log(router);
+
+router.post("/registration");
+router.get("/users");
+
+module.exports = router;
---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const PORT = process.env.PORT || 5000;
+const mongoose = require("mongoose");
+const authRouter = require("./authRouter");
+
+const app = express();
+app.use(express.json());
+app.use("/auth", authRouter);
+const start = async () => {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://admin:admin@cluster0.ndusm.mongodb.net/Cluster0?retryWrites=true&w=majority`
+    );
+    app.listen(PORT, () => console.log("server started"));
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+start();
